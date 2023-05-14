63584d40091a4c0cd0d368c848308c574f1c9537
MadhumitaPaul <92804024+MadhumitaPaul@users.noreply.github.com>
2023-05-14 20:35:38 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,7 @@
+class helloworld {
+
+	public static void main (String[] args) {
+		// Write your code here
+		System.out.println("how are you");
+	}
+}
---
+++ c.java
@@ -0,0 +1,7 @@
+class helloworld {
+
+	public static void main (String[] args) {
+		// Write your code here
+		System.out.println("Hope you are doing good.....");
+	}
+}
